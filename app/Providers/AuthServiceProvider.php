82bcc95383d778d0cadfcc397442fcc662551b7d
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2021-09-04 13:42:01 +0200

    Enhance authorization for users to show channel

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Channel;
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -25,6 +27,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('show-channel', function (User $user, Channel $channel) {
+            return $user->id === $channel->user_id;
+        });
     }
 }
